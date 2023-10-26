9b38ffb235e376f742243b56bcf3bba21824650a
zeqingwang <61963896+zeqingwang@users.noreply.github.com>
2023-10-26 11:43:58 -0700

    tutorial test

---
+++ FiveBigBrains/Assets/Scripts/UI/TutoralSceneManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class TutoralSceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ReturnToMainMenu()
+    {
+        Debug.Log("Returning to Main Menu...");
+        // if (GameManager.instance != null)
+        // {
+        //     GameManager.instance.RestartGameAfterVictory();
+        // }
+    }
+}
